41017ef4c657aa8d84cf3e36343d11c1f59a7eeb
aruzhan-zhakaibekova <az1788@nyu.edu>
2022-05-03 19:10:00 +0400

    First page/first video completed

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 $(document).ready(function() {
-
     setTimeout(function(){
         $('body').addClass('loaded');
-        $('h1').css('color','#222222');
     }, 3000);
 
 });
+
+
+
+$(document).ready(function() {
+  var btn = $(".button");
+  btn.click(function() {
+    btn.toggleClass("paused");
+    return false;
+  });
+});
+
+
+var video = document.querySelector('#video1');
+var btn = document.querySelector('.button');
+
+btn.addEventListener('click', function(){
+  if (video.paused) {
+      video.play();
+      }
+  else  {
+      video.pause();
+      }
+})
+
+var next = document.querySelector('.next');
+video.addEventListener("ended", function() {
+   next.style.display = "flex";
+   btn.classList.remove("paused");
+});
+
+next.onclick= function(){
+   location.href = "interrogation.html";
+};
